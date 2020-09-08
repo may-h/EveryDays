b57e95b2dda7b13da70dc40e6acc17a36f428970
may <hms9210@naver.com>
2020-09-08 23:59:37 +0900

    Frist Commit

---
+++ models/admin.js
@@ -0,0 +1,39 @@
+const { sequelize } = require(".");
+const { Sequelize, DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataTypes) => {
+ //모델이름은 단수형, 테이블 이름은 복수형 
+    return sequelize.define('admin', {
+        admin_no : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+        },
+        admin_id: { 
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+        },
+        company_name: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        company_code: {
+            type: DataTypes.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }
+    }, {
+        timestamps: true,
+        paranoid: true
+    })
+}
