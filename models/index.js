a3387502452556668187ca987f959a21c3221f70
may-h <hms9210@naver.com>
2020-09-11 09:49:27 +0900

    successfully connected mysql

--- models/index.js
+++ models/index.js
@@ -1,18 +1,22 @@
-const Sequelize = require('sequelize'); 
+const Sequelize = require("sequelize");
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config.json")[env];
 
-// console.log('hi')
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.paswsword, config);
-// console.log(sequelize)
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.paswsword,
+  config
+);
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = require('./user')(sequelize, require('sequelize/types'));
-// db.Admin = require('./admin')(sequelize, Sequelize); 
-// db.Payment = require('./payment')(sequelize, Sequelize);
+db.User = require("./user")(sequelize, Sequelize);
+db.Admin = require("./admin")(sequelize, Sequelize);
+db.Payment = require("./payment")(sequelize, Sequelize);
 
 module.exports = db;
--- models/user.js
+++ models/user.js
@@ -1,28 +1,32 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('user',{
-        user_no: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            unique: true,
-        },
-        name: {
-            type: DataTypes.STRING(20),
-            allowNull: false, 
-        },
-        phone: {
-            type: DataTypes.STRING(20),
-            allowNull: false
-        },
-        birth: {
-            type: DataTypes.DATE,
-            allowNull: true,
-        },
-        company_code: {
-            type: DataTypes.STRING(20),
-            allowNull: false
-        }
-    }, {
-        timestamps: true,
-        paranoid: true
-    })
-}
+module.exports = (sequelize, { DataTypes }) => {
+  return sequelize.define(
+    "user",
+    {
+      user_no: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      birth: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      company_code: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+};
